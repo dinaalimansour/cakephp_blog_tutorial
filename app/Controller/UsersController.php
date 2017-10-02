5221d5f8839ca22492de39fc464a4b2bbde89d36
dina <dinamansour.933@gmail.com>
2017-10-02 12:53:15 +0300

    add function

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -89,4 +89,8 @@ class UsersController extends AppController {
           
         return $this->redirect($this->Auth->logout());
     }
+    public function logout_test() {
+          
+        return $this->redirect($this->Auth->logout());
+    }
 }
